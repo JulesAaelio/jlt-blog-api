deaf59ac36d085f33f0d74a949ed758f37be5a4e
Jules LAURENT <jules.laurent@ynov.com>
2018-06-22 22:37:22 +0200

    get all / get one endpoints ready

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ const app = require('./config/express');
 const db = require('./config/sequelize');
 console.log("App started at  " , new Date().toLocaleString());
 
+app.use('/articles',require('./endpoints/articles')(db));
+
 app.listen(process.env.SRV_PORT);
