eb31f5c7f959ce7d9200e6c3d65ccc3a7d90a672
Jules LAURENT <jules.laurent@ynov.com>
2018-06-22 15:11:24 +0200

    configure database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const app = require('./config/express');
+const db = require('./config/sequelize');
 console.log("App started at  " , new Date().toLocaleString());
 
 app.listen(process.env.SRV_PORT);
