62c8df3bc40fbf10e5440fa2437b035882d35f0d
Jules LAURENT <jules.laurent@ynov.com>
2018-07-22 22:36:28 +0200

    add post endpoint for articles

--- src/endpoints/articles-protected.js
+++ src/endpoints/articles-protected.js
@@ -3,11 +3,20 @@ const express = require('express');
 module.exports = (db) => {
     const router = express.Router();
 
-    router.put('/:id',(req,res) => {
+    router.post('/', (req, res) => {
+        db.Article.create({
+            title: req.body.title,
+            content: req.body.content
+        }).then((article) => {
+            res.send(article);
+        })
+    });
+
+    router.put('/:id', (req, res) => {
         db.Article.findById(req.params.id).then(article => {
-            if(article) {
+            if (article) {
                 res.send(article);
-            }else {
+            } else {
                 res.status(404).send();
             }
         })
