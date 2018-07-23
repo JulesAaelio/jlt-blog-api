23cc28bee20bc22461abf06b50c66b61e10eb972
Jules LAURENT <jules.laurent@ynov.com>
2018-07-23 15:26:34 +0200

    expose image folder

--- src/config/express.js
+++ src/config/express.js
@@ -4,5 +4,6 @@ const bodyParser = require('body-parser');
 const app = new express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use('/public', express.static('public'));
 
 module.exports = app;
