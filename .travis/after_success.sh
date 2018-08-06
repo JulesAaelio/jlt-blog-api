83b551cf6feb141bcb5acbc72e5518c941c6325d
Jules LAURENT <jules.laurent@ynov.com>
2018-08-06 17:50:23 +0200

    add execution mode to travis scripts

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-git add -f .env && git commit -m "$TRAVIS_COMMIT_MESSAGE" # Forcefully add the ignored files.
+git add -f .env set_host_env.sh && git commit -m "$TRAVIS_COMMIT_MESSAGE" # Forcefully add the ignored files.
 git tag build/$TRAVIS_BUILD_NUMBER #Tag the commit so it's easy to roll back to previous build.
 git push deploy master
