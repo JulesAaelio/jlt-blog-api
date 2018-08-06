dbdcff919a79ff36a86ee61c292916f8bab34334
Jules LAURENT <jules.laurent@ynov.com>
2018-08-06 17:46:15 +0200

    improve continuous integration

---
+++ .travis/after_success.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git add -f .env && git commit -m "$TRAVIS_COMMIT_MESSAGE" # Forcefully add the ignored files.
+git tag build/$TRAVIS_BUILD_NUMBER #Tag the commit so it's easy to roll back to previous build.
+git push deploy master
--- set-env.js
+++ set-env.js
@@ -1,6 +1,6 @@
 const writeFile = require('fs').writeFile;
-console.log(process.env)
-const env = {
+console.log(process.env);
+const app_env = {
     DATABASE: process.env.DATABASE,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
@@ -10,22 +10,45 @@ const env = {
     OAUTH_SERVER: process.env.OAUTH_SERVER,
 };
 
-// const toWrite = 'export const environment = ' + JSON.stringify(env, null, '  ').replace(/\"([^(\")"]+)\":/g, '$1:') + ';';
-let toWrite ="";
-for (let prop in env) {
-    if (env.hasOwnProperty(prop)) {
-        toWrite += prop + '=';
-        console.log(typeof env[prop])
-        if(typeof env[prop] === 'string')
-            toWrite += '"' + env[prop] + '"';
-        else
-            toWrite += env[prop];
-        toWrite += '\n'
-    }
-}
+// const toWrite = 'export const environment = ' + JSON.stringify(app_env, null, '  ').replace(/\"([^(\")"]+)\":/g, '$1:') + ';';
+
+let toWrite = stringify(app_env);
 
 writeFile('.env', toWrite, (err => {
     if (err) {
         console.log(err);
     }
 }));
+
+
+const host_env = {
+    MYSQL_ROOT_PASSWORD: process.env.ROOT_PASSWORD,
+    MYSQL_USER: process.env.DB_USER,
+    MYSQL_PASSWORD: process.env.DB_PASSWORD
+    MYSQL_DATABASE: process.env.DATABASE,
+};
+toWrite = stringify(host_env, true);
+writeFile('set_host_env.sh', toWrite, (err => {
+    if (err) {
+        console.log(err);
+    }
+}));
+
+
+function stringify(env, shouldExport = false) {
+    let toWrite = "";
+    for (let prop in env) {
+        if (env.hasOwnProperty(prop)) {
+            if (shouldExport) {
+                toWrite += 'export '
+            }
+            toWrite += prop + '=';
+            if (typeof env[prop] === 'string')
+                toWrite += '"' + env[prop] + '"';
+            else
+                toWrite += env[prop];
+            toWrite += '\n'
+        }
+    }
+    return toWrite;
+}
