7157c386c4d7a39715342da41eeb808d0cc53b8b
Jules LAURENT <jules.laurent@ynov.com>
2018-08-07 22:41:31 +0200

    force checkout before deploying

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 git remote add deploy $deploy_uri
+git branch
+git checkout master -f
 git add -f .env set_host_env.sh && git commit -m "$TRAVIS_COMMIT_MESSAGE" # Forcefully add the ignored files.
 git tag build/$TRAVIS_BUILD_NUMBER #Tag the commit so it's easy to roll back to previous build.
 git push deploy master
