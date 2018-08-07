b2228c1c2dd48ea56d49cb712b00f5d06efd84a1
Jules LAURENT <jules.laurent@ynov.com>
2018-08-07 23:37:44 +0200

    force push to deploy

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -5,4 +5,4 @@ git branch
 git checkout master -f
 git add -f .env set_host_env.sh && git commit -m "$TRAVIS_COMMIT_MESSAGE" # Forcefully add the ignored files.
 git tag build/$TRAVIS_BUILD_NUMBER #Tag the commit so it's easy to roll back to previous build.
-git push deploy master
+git push --force deploy master
